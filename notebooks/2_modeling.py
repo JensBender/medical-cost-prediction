2dc9498ee91df7289a82ef18af65af5db1dcf52e
Jens Bender <jens.bender.001@gmail.com>
2026-03-19 13:44:43 +0100

    feat(notebook): create second notebook for modeling with linked ipynb to py via jupytext

---
+++ notebooks/2_modeling.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: Medical Cost Prediction
+#     language: python
+#     name: medical_cost_prediction
+# ---
+
+# %% [markdown]
+# <div style="text-align:center; background-color:#fff6e4; padding:20px; border:4px solid #f5ecda; border-radius:8px;">
+#     <div style="font-size:32px; font-weight:bold; color:#4A4A4A;">
+#         Medical Cost Prediction
+#     </div>
+#     <div style="font-size:24px; font-weight:bold; color:#4A4A4A;">
+#         Part 2: Modeling
+#     </div>
+#     <div style="font-size:14px; font-weight:normal; color:#666; margin-top:16px;">
+#         Author: Jens Bender <br> 
+#         Created: March 2026<br>
+#         Last updated: March 2026
+#     </div>
+# </div>
